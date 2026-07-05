8704cbb475b21920f307b36378f0bec35acba337
Ahmed Al-Tuwaijari <altuwaijari.ahmed@gmail.com>
2026-07-05 21:21:39 +0300

    Remove build artifacts, add build/ to gitignore

--- sysmon-dashboard/build/Desktop-Debug/CMakeFiles/sysmon_test.dir/sysmon_test_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sysmon_test.dir/sysmon_test_autogen/mocs_compilation.cpp.o: \
- /home/tuwai/sysmon-dashboard/sysmon-dashboard/build/Desktop-Debug/sysmon_test_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
